9ab41664f0ea8e7ca13110e65c6168f86e85b312
DenisaO <ondrovadenisa95@gmail.com>
2023-03-15 20:51:50 +0100

    homework 3

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('funguju!');
+const alarm = document.querySelector('.alarm');
+const audio = document.querySelector('audio');
+
+const seconds = prompt('Enter how many seconds should pass before the alarm rings');
+setTimeout(() => {
+  alarm.classList.add('alarm--ring');
+  audio.play();
+}, seconds * 1000);
+
+const cancelBtn = document.createElement('button');
+cancelBtn.textContent = 'Cancel';
+cancelBtn.addEventListener('click', () => {
+  clearTimeout(timer);
+  alarm.classList.remove('alarm--ring');
+});
+document.querySelector('.container').appendChild(cancelBtn);
